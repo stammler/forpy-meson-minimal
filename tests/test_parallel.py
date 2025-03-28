f6ea2abd9e6d5cab97f4218f4a08f23c39137e9a
Sebastian Stammler <sebastian.stammler@gmail.com>
2025-03-28 12:03:55 +0100

    Adding parallel tests

---
+++ tests/test_parallel.py
@@ -0,0 +1,7 @@
+from forpy_meson_minimal.fortran import parallel
+
+def test_in_parallel_true():
+    assert parallel.in_parallel_true() == 1
+
+def test_in_parallel_false():
+    assert parallel.in_parallel_false() == 0
