29bb64c5b1270096011b6c5be8aa4ccbbd8e320c
Sebastian Stammler <sebastian.stammler@gmail.com>
2025-03-28 18:06:38 +0100

    Adding timing example

---
+++ examples/timing.py
@@ -0,0 +1,34 @@
+import forpy_meson_minimal as forpy
+import numpy as np
+from timeit import default_timer
+
+N_threads = [2**i for i in range(4)]
+
+rng = np.random.default_rng()
+
+K = 5_000
+L = 3_000
+M = 2_000
+
+A = rng.random((K, M))
+B = rng.random((M, L))
+
+print()
+
+print("PARALLEL TIMING TEST")
+print("--------------------")
+
+print()
+
+print(f"Matrix-matrix multiplication:\n  C({K}x{L}) = A({K}x{M}) @ B({M}x{L})")
+
+print()
+
+for N in N_threads:
+    forpy.fortran.parallel.set_num_threads(N)
+    print(f"# Running with {forpy.fortran.parallel.n_threads:d} threads...")
+    t_start = default_timer()
+    C = forpy.matmul(A, B)
+    print(f"  - elapsed time: {default_timer()-t_start:.2f} s")
+
+print()
